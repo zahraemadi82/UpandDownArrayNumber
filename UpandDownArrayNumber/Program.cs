18f0efed26de18410d2e1478b4a9b62a2ac710f3
User <User@DESKTOP-1364VBG>
2024-01-29 17:36:12 +0330

    Add project files.

---
+++ UpandDownArrayNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] myArray = new int[100];
+
+for (int i = 0;i <= 100; i++)
+{
+    Console.Write($"Enter Number {i} : ");
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Max Number : {myArray.Max()}");
+Console.WriteLine($"Min Number : {myArray.Min()}");
